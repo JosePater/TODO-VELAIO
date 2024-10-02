96a4915c68228e6390016e47a41e6b868a017f32
PaterDev <josepater_1130@hotmail.com>
2024-10-01 22:35:05 -0500

    feat(navbar, routes): added navbar and router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule]
 })
-export class AppComponent {
-  title = 'TODO-VELAIO';
-}
+export class AppComponent {}
