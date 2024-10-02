ea3d04545ef73711b9e1102e9bf4df28c48c83d5
PaterDev <josepater_1130@hotmail.com>
2024-10-02 18:11:18 -0500

    feat: added validations for adding and removing people, adding and removing skills

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -55,29 +55,56 @@ export class HomeComponent {
     return this.associatedPersons.at(indexPerson).get('skills') as FormArray;
   }
 
-  // Si hay habilidades
-  isSkills(indexPerson: number): boolean {
-    const arraySkills = this.getSkills(indexPerson);
-    return arraySkills.controls.some((skill) => skill.valid);
+  // Valida si la skill se diligenció antes de agregar otra
+  isValidSkill(indexPerson: number): boolean {
+    // Obtiene el status de la habilidad (si se escribió)
+    const isValidPreviousSkill =
+      this.associatedPersons.controls[indexPerson].get('skills')?.valid;
+
+    // Si se escribió la habilidad se puede agregar otra
+    if (isValidPreviousSkill) {
+      const arraySkills = this.getSkills(indexPerson);
+      return arraySkills.controls.some((skill) => skill.valid);
+    }
+    return false;
   }
 
   // Agregar personas
   addPerson(): void {
-    const newPerson = this.formBuilder.group({
-      fullname: ['', [Validators.required, Validators.minLength(5)]],
-      age: ['', [Validators.required, Validators.min(18)]],
-      skills: this.formBuilder.array([
-        this.formBuilder.control('', Validators.required),
-      ]),
-    });
+    // Cantidad de personas
+    const lengthPersons: number = this.associatedPersons.controls.length;
+    const previousPerson: number = lengthPersons - 1; // Persona anterior
 
-    // Pendiente validar nombre único
-    this.associatedPersons.push(newPerson);
+    const isValidPreviousPerson: boolean =
+      this.associatedPersons.controls[previousPerson].valid;
+
+    // Si el registro de la persona anterior es válido, se podrá agregar otra
+    if (isValidPreviousPerson) {
+      const newPerson = this.formBuilder.group({
+        fullname: ['', [Validators.required, Validators.minLength(5)]],
+        age: ['', [Validators.required, Validators.min(18)]],
+        skills: this.formBuilder.array([
+          this.formBuilder.control('', Validators.required),
+        ]),
+      });
+
+      // Pendiente validar nombre único
+      this.associatedPersons.push(newPerson);
+    } else {
+      alert(`Diligencie correctamente los datos de la persona anterior`);
+    }
   }
 
   // Eliminar una persona
   removePerson(indexPerson: number): void {
-    this.associatedPersons.removeAt(indexPerson);
+    // Cantidad de personas asociadas
+    const lengthPersons: number = this.taskForm.value.associatedPersons.length;
+
+    if (lengthPersons === 1) {
+      alert('Debe haber al menos una persona asociada');
+    } else {
+      this.associatedPersons.removeAt(indexPerson);
+    }
   }
 
   // Agregar una habilidad en una persona
@@ -89,7 +116,16 @@ export class HomeComponent {
 
   // Eliminar una habilidad
   removeSkill(indexPerson: number, indexSkill: number): void {
-    this.getSkills(indexPerson).removeAt(indexSkill);
+    // Cantidad de skills de la persona especificada por su index
+    const lengthSkills: number =
+      this.taskForm.value.associatedPersons[indexPerson].skills.length;
+
+    // Si solo hay una habilidad, no se podrá eliminarla
+    if (lengthSkills === 1) {
+      alert('Debe haber al menos una habilidad');
+    } else {
+      this.getSkills(indexPerson).removeAt(indexSkill);
+    }
   }
 
   // Validar campos si con válidos
