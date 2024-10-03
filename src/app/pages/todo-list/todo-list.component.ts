92fb7d420b047a1266bfdeab18f8ccc593336db6
PaterDev <josepater_1130@hotmail.com>
2024-10-03 14:13:08 -0500

    feat: added task list page template

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { TodoService } from 'src/app/service/todo.service';
@@ -6,12 +7,15 @@ import { TodoService } from 'src/app/service/todo.service';
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css'],
+  standalone: true,
+  imports: [NgFor],
 })
 export class TodoListComponent {
   private _localStorageService = inject(TodoService); // Servicio localStorage
+  public allTask = this._localStorageService.getAllTasks();
 
   // Ver tareas de localStorage en la consola
   viewTask() {
-    console.log(this._localStorageService.getAllTasks());
+    console.log(this.allTask);
   }
 }
